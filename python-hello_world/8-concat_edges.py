ffcfe2bf67099bb26771100afbebae7ed0f7f837
Johnson <jamhalex@gmail.com>
2026-01-13 12:12:11 -0400

    task 8 deleted witespace

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# slice and rearrange
 str = (str[39:67] + str[106:112] + str[:6]).strip()
 print(str)
