0ade17bde330330dd723510288180c0f9c41a55c
Johnson <jamhalex@gmail.com>
2026-01-13 12:22:00 -0400

    task 8 added space between lines

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[39:67] + str[105:111] + str[:6]).strip()
+str = str[39:67] + str[109:115] + str[:6]
 print(str)
