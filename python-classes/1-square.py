88e904b415d4a3525c439706930f326b32d2e0c7
Johnson <jamhalex@gmail.com>
2026-02-02 06:09:00 -0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a  Square class with a private size attribute."""
+
+
+class Square:
+    def __init___(self, size):
+        """Initialize a Square with a private size"""
+        self.__size = size
