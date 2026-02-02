cbd0cd0dd69a8474818025ba6cfc0ce17bb1aebd
Johnson <jamhalex@gmail.com>
2026-02-02 06:30:54 -0400

    task 1 rework

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a  Square class with a private size attribute."""
+"""Defines a Square class with a private size attribute."""
 
 
 class Square:
-    """Represents a square"""
+    """Represents a square."""
 
-    def __init___(self, size):
-        """Initialize a Square with a private size"""
+    def __init__(self, size):
+        """Initialize a Square with a private size."""
         self.__size = size
