3e9f729b6bf4340693fe756108be6186d73d0c87
Johnson <jamhalex@gmail.com>
2026-02-02 00:14:29 -0400

    negated non integer values

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,12 +5,20 @@
 def add_integer(a, b=98):
     """Return the addition of a and b as integers.
 
-    a and b must be integers or floats, otherwise raise TypeError.
+    a and b must be integers or floats.
     Floats are casted to integers before addition.
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
+    # Reject NaN values (float overflow case)
+    if a != a:
+        raise TypeError("a must be an integer")
+    if b != b:
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
+
