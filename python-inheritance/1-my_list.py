671739aebf15a4769bffeb755988a89f119c1e01
Johnson <jamhalex@gmail.com>
2026-02-24 06:06:12 -0400

    task 1 / test folder

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    Inherits from list and adds a method to print the list sorted.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+        """
+        print(sorted(self))
