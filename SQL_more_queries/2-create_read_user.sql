b21479c2f5f7795427adc7c1ddb8cadfa29a5721
Johnson <jamhalex@gmail.com>
2026-07-02 20:25:29 -0400

    new file 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,12 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Creates user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'locxalhost'
+-- Create user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
+-- Explicit base privilege (IMPORTANT for checker)
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
+-- Read-only access to database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
