afeace2639f3a90bf74813a6b550e4d8debccf20
Johnson <jamhalex@gmail.com>
2026-07-02 21:17:16 -0400

    ++sql queries

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- creates hbtn_0d_usa database and cities table with foreign keys to states
+CREATE DATABASEE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state id) REFERENCES states (id)
+);
