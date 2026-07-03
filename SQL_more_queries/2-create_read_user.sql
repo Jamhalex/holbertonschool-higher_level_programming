dfb0db0be0251b1b4a9014f00d4d542770d3b3f1
Johnson <jamhalex@gmail.com>
2026-07-02 20:17:32 -0400

    fixed typo

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'locxalhost'
 IDENTIFIED BY 'user_0d_2_pwd";
 
--- Grant selected permissions
+-- Grants read  privileges
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-\
