f292f4ec05b0184fb50aafbf09033ea14800f0e0
Johnson <jamhalex@gmail.com>
2026-07-02 20:13:42 -0400

    user w read priv.

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+
+-- Creates user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'locxalhost'
+IDENTIFIED BY 'user_0d_2_pwd";
+
+-- Grant selected permissions
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+\
